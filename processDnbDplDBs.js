4f2c8b4145bce9016274f3fcd36d10535e74a24d
Hans de Rooij <hansderooij@hotmail.com>
2022-08-08 19:25:13 +0000

    Added rate limiter to data block processing algo

--- processDnbDplDBs.js
+++ processDnbDplDBs.js
@@ -22,8 +22,24 @@
 
 import * as path from 'path';
 import { promises as fs } from 'fs';
+import { RateLimiter } from 'limiter';
+
+const readFileLimiter = new RateLimiter({ tokensPerInterval: 50, interval: 'second' });
 
 const filePath = { root: '', dir: 'out' };
+
+function addToCharacterSeparatedRow(value) {
+    const separator = '|';
+
+    switch(typeof value) {
+        case 'string':  return value + separator;
+        case 'boolean': return Boolean.toString(value) + separator;
+        case 'number':  return Number.toString(value) + separator;
+        case 'null':
+        case 'undefined': console.log('Input value is null or undefined');
+        default: return separator; 
+    }
+}
 
 fs.readdir(path.format(filePath))
     .then(arrFiles => {
@@ -32,27 +48,38 @@ fs.readdir(path.format(filePath))
                         .filter(fn => fn.indexOf('dnb_dpl_full_fam') === -1);
 
         arrFiles.forEach(fn => {
-            fs.readFile(path.format({ ...filePath, base: fn }))
-                .then(file => {
-                    let dbs;
+            readFileLimiter.removeTokens(1)
+                .then(() => {
+                    fs.readFile(path.format({ ...filePath, base: fn }))
+                        .then(file => {
+                            let dbs;
 
-                    try {
-                        dbs = JSON.parse(file)
-                    }
-                    catch(err) {
-                        console.error(err.message);
-                        return;
-                    }
+                            try {
+                                dbs = JSON.parse(file)
+                            }
+                            catch(err) {
+                                console.error(err.message);
+                                return;
+                            }
 
-                    const org = dbs.organization;
+                            const org = dbs.organization;
 
-                    let retStr = '';
+                            let retStr = '';
 
-                    retStr += org.duns + '|';
-                    retStr += org.primaryName + '|';
-                    retStr += org.countryISOAlpha2Code;
+                            retStr += addToCharacterSeparatedRow(org.duns);
+                            retStr += addToCharacterSeparatedRow(org.primaryName);
+                            retStr += addToCharacterSeparatedRow(org.countryISOAlpha2Code);
 
-                    console.log(retStr);
+                            if(org.dunsControlStatus && org.dunsControlStatus.operatingStatus) {
+                                retStr += addToCharacterSeparatedRow(org.dunsControlStatus.operatingStatus.description)
+                            }
+                            else {
+                                retStr += addToCharacterSeparatedRow('')
+                            }
+
+                            console.log(retStr.slice(0, -1));
+                        })
+                        .catch(err => console.error(err.message))
                 })
                 .catch(err => console.error(err.message))
         });
