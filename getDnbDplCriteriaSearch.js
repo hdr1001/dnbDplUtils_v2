643eeb858f26f45c0fef217365b1486c552a790a
Hans de Rooij <hansderooij@hotmail.com>
2022-08-16 17:48:47 +0000

    Add persistence and mult country downloads

--- getDnbDplCriteriaSearch.js
+++ getDnbDplCriteriaSearch.js
@@ -20,21 +20,35 @@
 //
 // *********************************************************************
 
+import * as path from 'path';
+import { promises as fs } from 'fs';
 import { 
    httpCriteriaSearch,
    ReqDnbDpl,
 } from './dnbDplLib.js';
 
+const filePathOut = { root: '', dir: 'out' };
+
 const oCriteria = {
-   countryISOAlpha2Code: 'NL',
-   addressLocality: 'Delft',
-   usSicV4: ['5812'],
+   numberOfEmployees: { informationScope: 9066, minimumValue: 5, maximumValue: 25},
+   usSicV4: ['7538'],
    isOutOfBusiness: false
 }
 
-new ReqDnbDpl(httpCriteriaSearch, null, oCriteria)
-   .execReq('Executing a criteria search')
-      .then(resp => {
-         console.log(resp.buffBody.toString())
-      })
-      .catch(err => console.error(err.message))
+const countries = ['AU', 'AT', 'BE', 'BA', 'BG', 'CA', 'CN', 'HR', 'CZ', 'DK', 'EE', 'FI', 'FR',
+                     'DE', 'GR', 'HU', 'IN', 'IE', 'IT', 'JP', 'XK', 'LV', 'LT', 'LU', 'MK', 'MT',
+                     'ME', 'NL', 'NO', 'PL', 'PT', 'CY', 'RO', 'RS', 'SK', 'SI', 'ES', 'SE', 'CH',
+                     'GB', 'US'];
+
+countries.forEach(ctry => 
+   new ReqDnbDpl(httpCriteriaSearch, null, { ...oCriteria, countryISOAlpha2Code: ctry})
+      .execReq('Executing a criteria search')
+         .then(resp => {
+            const base = `criteriaSearch_${ctry}${resp.httpStatus === 200 ? '' : `_${resp.httpStatus}`}.json`;
+
+            fs.writeFile(path.format({ ...filePathOut, base }), resp.buffBody)
+               .then( /* console.log(`Wrote file ${base} successfully`) */ )
+               .catch(err => console.error(err.message))
+         })
+         .catch(err => console.error(err.message))
+);
